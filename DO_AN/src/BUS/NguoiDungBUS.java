4558063daa7ffd658c4e3b9cbc1e17f36a56eb78
HaiBao <147289505+HaiBao1728@users.noreply.github.com>
2023-10-30 17:58:06 +0700

    Update NguoiDungBUS.java

--- DO_AN/src/BUS/NguoiDungBUS.java
+++ DO_AN/src/BUS/NguoiDungBUS.java
@@ -19,4 +19,65 @@ public class NguoiDungBUS {
 		NguoiDungDAO ndDAO = new NguoiDungDAO();
 		return ndDAO.getNguoiĐungangNhap(userName, Password);
 	}
+	NguoiDungDAO nd = new NguoiDungDAO();
+
+    public Vector<NguoiDung> getAllUser(){
+        return nd.getAllUser();
+    }
+
+    public Vector<NguoiDung> selectUserPer(String quyen){
+        return nd.selectUserPer(quyen);
+    }
+
+    public Vector<String> getQuyen(){
+        return nd.getQuyen();
+    }
+
+    public Vector<String> getQuyenEdit(){
+        return nd.getQuyenEdit();
+    }
+
+    public boolean deleAcc(String id){
+        return nd.deleAcc(id);
+    }
+
+    public NguoiDung getSelectUser(String id){
+        return nd.getSelectUser(id);
+    }
+
+    public boolean editAcc(String id, String acc){
+        if (acc.matches("[a-z0-9_-]{4,10}$")){
+            return nd.editAcc(id, acc);
+        }
+        else{
+            return false;
+        }
+    }
+
+    public boolean editName(String id, String acc){
+        if (acc.matches("[a-zỳọáầảấờễàạằệếýộậốũứĩõúữịỗìềểẩớặòùồợãụủíỹắẫựỉỏừỷởóéửỵẳẹèẽổẵẻỡơôưăêâđ' A-ZZỲỌÁẦẢẤỜỄÀẠẰỆẾÝỘẬỐŨỨĨÕÚỮỊỖÌỀỂẨỚẶÒÙỒỢÃỤỦÍỸẮẪỰỈỎỪỶỞÓÉỬỴẲẸÈẼỔẴẺỠƠÔƯĂÊÂĐ']{2,50}$")){
+            return nd.editName(id, acc);
+        }
+        else{
+            return false;
+        }
+    }
+
+    public boolean editPass(String id, String acc){
+        if (acc.matches("^(?=.*[A-Za-z].*)(?=.*[0-9].*)[A-Za-z0-9]{10,32}$")){
+            return nd.editPass(id, acc);
+        }
+        else{
+            return false;
+        }
+    }
+
+    public boolean editMail(String id, String acc){      
+        if (acc.matches("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$")){
+            return nd.editMail(id, acc);
+        }
+        else{
+            return false;
+        }
+    }
 }
